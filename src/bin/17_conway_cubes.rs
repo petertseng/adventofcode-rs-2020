95f2d609f7631a015b75053b6aff072bed4256dd
Peter Tseng <pht24@cornell.edu>
2020-12-19 13:40:29 +0000

    day 17: Use w=z symmetry

--- src/bin/17_conway_cubes.rs
+++ src/bin/17_conway_cubes.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 type Dim = u16;
 type Time = u16;
 type Coord = i32;
@@ -9,39 +11,45 @@ type Pos = i64;
 // u32 will support up to 20 dimensions (3 ** 20 < 2 ** 32 - 1)
 type NeighCount = u32;
 
-fn step(now_active: &[Pos], dimensions: Dim, dposes: &[Pos], wzbits: usize) -> Vec<Pos> {
-    use std::collections::HashMap;
+type NeighMap = HashMap<Pos, HashMap<Pos, NeighCount>>;
+type CollapsedNeighMap = HashMap<Pos, Vec<(Pos, NeighCount)>>;
 
+fn step(
+    now_active: &[Pos],
+    dimensions: Dim,
+    weights: &CollapsedNeighMap,
+    ybits: usize,
+    wzbits: usize,
+) -> Vec<Pos> {
     // (neighbour count << 1) | self
     let mut neigh_and_self: HashMap<Pos, NeighCount> = HashMap::new();
-
-    // Hmm, if I put this inside the loop,
-    // would the compiler optimise it?
-    let mut wz = vec![0; usize::from(dimensions) - 2];
+    let wzshift = wzbits * (usize::from(dimensions) - 2);
+    let wzmask = (1_i64 << wzshift) - 1;
+    let pos_per_dy = 1_i64 << wzshift;
+    let pos_per_dx = pos_per_dy << ybits;
+    let dxys: Vec<_> = repeated_permutation(&[0, -1, 1], 2)
+        .iter()
+        .map(|dxy| dxy[0] * pos_per_dx + dxy[1] * pos_per_dy)
+        .collect();
 
     for &pos in now_active {
-        let mut tmppos = pos;
-        for i in 0..dimensions - 2 {
-            wz[usize::from(i)] = (tmppos & ((1 << wzbits) - 1)) as Coord;
-            tmppos >>= wzbits;
+        for (nwz, weight) in &weights[&(pos & wzmask)] {
+            let npos = pos & !wzmask | nwz;
+            for dxy in &dxys {
+                *neigh_and_self.entry(npos + dxy).or_insert(0) += weight << 1;
+            }
         }
-        for dpos in dposes {
-            let npos = pos + dpos;
-            let mut tmpnpos = npos;
-            let mut count = 2;
-            for i in 0..dimensions - 2 {
-                let c = tmpnpos & ((1 << wzbits) - 1);
-                tmpnpos >>= wzbits;
-                if c == 0 {
-                    // negative coordinate (remember we offset by 1)
-                    count *= 0;
-                } else if c == 1 && wz[usize::from(i)] == 2 {
-                    count *= 2;
-                }
-            }
-            if count != 0 {
-                *neigh_and_self.entry(npos).or_insert(0) += count;
-            }
+        // for e.g. [x, y, z, w] -> [x + 1, y, z, w]
+        // NOTE that if a cell is a representative of one of its own neighbours,
+        // e.g, [x, y, 0, 1] -> [x, y, 1, 0] (which is represented by [x, y, 0, 1]),
+        // the above weights will already have included that fact.
+        // This is only for the single extra neighbour for nonequal [x, y].
+        // TODO: Actually, it'd be possible to include the extra 1 in the neighbour map,
+        // and then subtract the extra neighbour from pos...
+        // You'd think runtime would benefit from avoiding this extra loop,
+        // but it didn't seem to make an actual difference when I tried it?
+        for dxy in &dxys[1..] {
+            *neigh_and_self.entry(pos + dxy).or_insert(0) += 1 << 1;
         }
     }
     for &pos in now_active {
@@ -100,29 +108,108 @@ fn decompress(
     coord
 }
 
-fn neigh(dimensions: Dim, ybits: usize, wzbits: usize) -> Vec<Pos> {
+fn neigh_weights(dimensions: Dim, rounds: Time, wzbits: usize) -> CollapsedNeighMap {
+    let mut weights = HashMap::new();
     // 0 will be first from repeated_permutation, so drop it with [1..]
-    let coords = &repeated_permutation(&[0, -1, 1], usize::from(dimensions))[1..];
-    coords
-        .iter()
-        .map(|coord| compress(coord[0], coord[1], &coord[2..], 0, 0, ybits, wzbits))
+    let ds = &repeated_permutation(&[0, -1, 1], usize::from(dimensions - 2))[1..];
+
+    // Recursive closure pattern:
+    // https://stackoverflow.com/questions/16946888/is-it-possible-to-make-a-recursive-closure-in-rust
+    struct BuildIfRepresentative<'s> {
+        f: &'s dyn Fn(&BuildIfRepresentative, Dim, &mut [Coord], &mut NeighMap),
+    }
+    let build_if_representative = BuildIfRepresentative {
+        f: &|build_if_rep, n, prefix, weights| {
+            if n == dimensions - 2 {
+                neigh_weights_for(prefix, ds, rounds, wzbits, weights);
+            } else {
+                let last = if n == 0 {
+                    0
+                } else {
+                    prefix[usize::from(n) - 1]
+                };
+                for x in last..=Coord::from(rounds) {
+                    prefix[usize::from(n)] = x;
+                    (build_if_rep.f)(build_if_rep, n + 1, prefix, weights);
+                }
+            }
+        },
+    };
+    let mut prefix = vec![0; usize::from(dimensions) - 2];
+    (build_if_representative.f)(&build_if_representative, 0, &mut prefix, &mut weights);
+
+    weights
+        .into_iter()
+        .map(|(k, v)| (k, v.into_iter().collect::<Vec<_>>()))
         .collect()
 }
 
+fn neigh_weights_for(
+    pt: &[Coord],
+    ds: &[Vec<Coord>],
+    rounds: Time,
+    wzbits: usize,
+    h: &mut NeighMap,
+) {
+    assert!(is_representative(pt));
+    let comp_pt = compress(0, 0, pt, 0, 1, 0, wzbits);
+    for d in ds {
+        let npt: Vec<_> = (0..pt.len()).map(|i| pt[i] + d[i]).collect();
+        // points with any coordinate equal to # rounds only appear in the last iteration,
+        // so we don't need to compute their outgoing neighbours
+        if npt.iter().any(|n| n.abs() >= Coord::from(rounds)) {
+            continue;
+        }
+        let comp_neigh_rep = compress(0, 0, &representative(&npt), 0, 1, 0, wzbits);
+        *h.entry(comp_neigh_rep)
+            .or_insert_with(HashMap::new)
+            .entry(comp_pt)
+            .or_insert(0) += 1;
+    }
+}
+
+fn representative(pt: &[Coord]) -> Vec<Coord> {
+    let mut pt: Vec<_> = pt.iter().map(|a| a.abs()).collect();
+    pt.sort_unstable();
+    pt
+}
+
+fn is_representative(pt: &[Coord]) -> bool {
+    for &c in pt {
+        if c < 0 {
+            return false;
+        }
+    }
+    for i in 1..pt.len() {
+        if pt[i - 1] > pt[i] {
+            return false;
+        }
+    }
+    true
+}
+
 fn size(compressed: &[Pos], dimensions: Dim, wzbits: usize) -> u64 {
+    let perms_wz: u64 = (1..=u64::from(dimensions - 2)).product();
+
     compressed
         .iter()
         .map(|pos| {
             let mut count = 1_u64;
             let mut tmppos = *pos;
+            let mut tally = HashMap::new();
             for _ in 0..dimensions - 2 {
                 let coord = tmppos & ((1 << wzbits) - 1);
+                *tally.entry(coord).or_insert(0) += 1;
                 if coord != 1 {
                     count *= 2;
                 }
                 tmppos >>= wzbits;
             }
-            count
+            count * perms_wz
+                / tally
+                    .values()
+                    .map(|&v| (1_u64..=v).product::<u64>())
+                    .product::<u64>()
         })
         .sum()
 }
@@ -212,7 +299,7 @@ fn main() {
 
     for dim in dims {
         let t1 = Instant::now();
-        let dposes = neigh(dim, ybits, wzbits);
+        let weights = neigh_weights(dim, time, wzbits);
         let elapsed_neigh = t1.elapsed();
 
         let zs = vec![0; usize::from(dim) - 2];
@@ -233,7 +320,7 @@ fn main() {
 
         let t2 = Instant::now();
         for _t in 1..=time {
-            active = step(&active, dim, &dposes, wzbits);
+            active = step(&active, dim, &weights, ybits, wzbits);
             if false {
                 println!("t={} {}", _t, size(&active, dim, wzbits));
                 println!("t={} {:?}", _t, active);
