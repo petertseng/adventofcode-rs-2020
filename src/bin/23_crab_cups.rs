11d18b6274aa6adbfdf434f7f7266269e59d62a1
Peter Tseng <pht24@cornell.edu>
2020-12-23 21:15:42 +0000

    day 23: Use Vec<u32>

--- src/bin/23_crab_cups.rs
+++ src/bin/23_crab_cups.rs
@@ -1,33 +1,35 @@
-fn game(cups: &[usize], ncups: usize, rounds: usize, after1: usize) -> Vec<usize> {
+// All the `as` casts are regrettable,
+// but using Vec<u32> does make the code faster than Vec<usize>
+fn game(cups: &[u8], ncups: u32, rounds: usize, after1: u32) -> Vec<u64> {
     let mut right: Vec<_> = (1..=(ncups + 1)).into_iter().collect();
     for (&l, &r) in cups.iter().zip(cups.iter().skip(1)) {
-        right[l] = r;
+        right[usize::from(l)] = u32::from(r);
     }
-    if ncups > cups.len() {
-        right[cups[cups.len() - 1]] = cups.len() + 1;
-        right[ncups] = cups[0];
+    if ncups as usize > cups.len() {
+        right[cups[cups.len() - 1] as usize] = cups.len() as u32 + 1;
+        right[ncups as usize] = u32::from(cups[0]);
     } else {
-        right[cups[cups.len() - 1]] = cups[0];
+        right[cups[cups.len() - 1] as usize] = u32::from(cups[0]);
     }
 
-    let mut current = cups[0];
+    let mut current = u32::from(cups[0]);
 
     for _ in 0..rounds {
-        let pickup1 = right[current];
-        let pickup2 = right[pickup1];
-        let pickup3 = right[pickup2];
-        let after_pickup = right[pickup3];
+        let pickup1 = right[current as usize];
+        let pickup2 = right[pickup1 as usize];
+        let pickup3 = right[pickup2 as usize];
+        let after_pickup = right[pickup3 as usize];
 
         let mut dest = if current == 1 { ncups } else { current - 1 };
         while dest == pickup1 || dest == pickup2 || dest == pickup3 {
             dest = if dest == 1 { ncups } else { dest - 1 };
         }
 
-        let right_of_dest = right[dest];
+        let right_of_dest = right[dest as usize];
 
-        right[current] = after_pickup;
-        right[dest] = pickup1;
-        right[pickup3] = right_of_dest;
+        right[current as usize] = after_pickup;
+        right[dest as usize] = pickup1;
+        right[pickup3 as usize] = right_of_dest;
 
         current = after_pickup;
     }
@@ -35,7 +37,7 @@ fn game(cups: &[usize], ncups: usize, rounds: usize, after1: usize) -> Vec<usize
     let mut current = 1;
     (0..after1)
         .map(|_| {
-            let x = right[current];
+            let x = u64::from(right[current as usize]);
             current = x;
             x
         })
@@ -53,12 +55,12 @@ fn main() {
     };
     let cups: Vec<_> = number
         .chars()
-        .filter_map(|c| c.to_digit(10).map(|c| c as usize))
+        .filter_map(|c| c.to_digit(10).map(|c| c as u8))
         .collect();
 
     println!(
         "{}",
-        game(&cups, cups.len(), 100, cups.len() - 1)
+        game(&cups, cups.len() as u32, 100, cups.len() as u32 - 1)
             .iter()
             .fold(0, |a, x| a * 10 + x)
     );
@@ -66,6 +68,6 @@ fn main() {
         "{}",
         game(&cups, 1_000_000, 10_000_000, 2)
             .iter()
-            .product::<usize>()
+            .product::<u64>()
     );
 }
